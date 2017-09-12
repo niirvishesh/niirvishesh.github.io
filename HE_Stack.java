c7477028d1d011d365d452cc27516a3fd778a20a
Nirvishesh Shrivastava <31612080+niirvishesh@users.noreply.github.com>
2017-09-12 20:12:44 +0530

    Update HE_Stack.java

--- HE_Stack.java
+++ HE_Stack.java
@@ -1,55 +1,44 @@
 import java.util.*;
-class Solution
+class TestClass 
 {
-	public static void main(String args[])
-	{
-		Scanner in = new Scanner(System.in);
-				// 	No of coins: 
-		int n = in.nextInt(),sum=0,flag=0;
-		Integer ar[] = new Integer[n];
-		Stack<Integer> a = new Stack<Integer>();
-		Stack<Integer> b = new Stack<Integer>();
-				// 	Enter gold coins: 
-		for(int i=0;i<n;i++)
-		{
-			a.push(ar[i]=in.nextInt());
-		}
-		
-				// 	No. of Instructions: 
-		int ins = in.nextInt();
-				// 	Amount monk should have:
-		int at = in.nextInt();
-				// 	Enter the instructions: 
-		String str[] = new String[ins];
-		for(int i=0;i<ins;i++)
-		{
-			str[i]=in.next();
-		}
-		
-		aa:	
-		for(int i=0;i<ins;i++)
-		{
-			if(str[i].equals("Harry"))
-			{
-				sum+=b.push(a.pop());
-				if(sum==at)
-				{
-					flag=1; break aa;
-				}
-			}
-			if(str[i].equals("Remove"))
-			{
-				sum-=b.pop();
-				if(sum==at)
-				{
-					flag=1; break aa;
-				}
-			}
-		}
-		
-		if(flag==1)
-			System.out.println(b.size());
-		else
-			System.out.println("-1");
-	}
+    public static void main(String args[] )
+    {
+ 
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        Stack<Integer> s=new Stack<>();
+        int[] a=new int[N];
+        for(int i=0;i<N;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        int Q=sc.nextInt();
+        int X=sc.nextInt();
+        int in=0,sum=0;
+        sc.nextLine();
+        for(int l=0;l<Q;l++)
+        {
+            String st=sc.nextLine();
+            if(st.equals("Harry"))
+            {
+                s.push(a[in]);
+                sum+=a[in];
+                in++;          
+            }
+            if(st.equals("Remove"))
+            {
+                int x=s.pop();
+                sum=sum-x;
+            }
+            
+            if(sum==X)
+            {
+                System.out.println(s.size());
+                break;
+            }
+            
+        }
+        if(sum!=X)
+            System.out.println(-1);
+    }
 }
